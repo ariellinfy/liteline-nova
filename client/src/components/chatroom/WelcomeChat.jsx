4b825e5cb57feff17ad3a45fee6ef8b99688dec9
Ariel Lin <72408793+ariellinfy@users.noreply.github.com>
2025-08-15 14:37:59 -0600

    Start from fresh

--- client/src/components/chatroom/WelcomeChat.jsx
+++
@@ -1,9 +0,0 @@
-const WelcomeChat = () => {
-  return (
-    <div className="flex w-full justify-center items-center">
-      Select or join a room to start chatting!
-    </div>
-  );
-};
-
-export default WelcomeChat;
--- client/src/socket.js
+++
@@ -1,9 +0,0 @@
-import io from "socket.io-client";
-
-const socket = io(process.env.REACT_APP_Proxy_EndPoint, {
-  autoConnect: false,
-  withCredentials: true,
-  transports: ["websocket"],
-});
-
-export default socket;
